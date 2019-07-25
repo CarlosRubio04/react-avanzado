9b08c0a8fd28bb9920b60ce9d00b5a30ad6789b8
Carlos Rubio <crubio@acsendo.com>
2019-07-25 07:18:02 -0500

    React Icons, lista de foto cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { GlobalStyles } from './GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
+import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+
 export const App = () => (
   <div>
     <GlobalStyles />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </div>
 )
