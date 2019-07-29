363201ac00dcc3fd2a373583428271b0e8763359
Carlos Rubio <crubio@acsendo.com>
2019-07-29 07:30:06 -0500

    UseEffect hook

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
   const [categories, setCatecories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
   useEffect(function () {
     window.fetch('https://petgram-server.midudev.now.sh/categories')
@@ -12,11 +13,29 @@ export const ListOfCategories = () => {
         setCatecories(response)
       })
   }, [])
-  return (
-    <List>
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+  return (
+    <Fragment>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </Fragment>
   )
 }
