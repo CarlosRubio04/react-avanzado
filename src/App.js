2c831821b5765dd3c3ce4f091e43729ec332b84c
Carlos Rubio <crubio@acsendo.com>
2019-07-31 09:08:17 -0500

    Render props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { GlobalStyles } from './styles/GlobalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './container/ListOfPhotoCards'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 
-export const App = () => (
-  <div>
-    <Logo />
-    <GlobalStyles />
-    <ListOfCategories />
-    <ListOfPhotoCards categoryId={3} />
-  </div>
-)
+export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
+  return (
+    <div>
+      <GlobalStyles />
+      <Logo />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <Fragment>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={2} />
+          </Fragment>
+      }
+    </div>
+  )
+}
