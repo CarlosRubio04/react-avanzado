88a3259a0540747c2d669dc8c448b1a87c3cd897
Carlos Rubio <crubio@acsendo.com>
2019-08-05 07:16:56 -0500

    Paginas, navbar estilado y rutas protegidas

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { GlobalStyles } from './styles/GlobalStyles'
 
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Logo } from './components/Logo'
-
+import { NavBar } from './components/NavBar'
 import { Router } from '@reach/router'
 
+const UserLogged = ({ children }) => {
+  return children({ isAth: false })
+}
 export const App = () => {
   return (
     <div>
@@ -17,6 +23,21 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAth }) =>
+            isAth
+              ? <Router>
+                <Favs path='/favs' />
+                <User path='/user' />
+              </Router>
+              : <Router>
+                <NotRegisteredUser path='/favs' />
+                <NotRegisteredUser path='/user' />
+              </Router>
+        }
+      </UserLogged>
+      <NavBar />
     </div>
   )
 }
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const NotRegisteredUser = () => (
+  <h1>Hola Registro</h1>
+)
