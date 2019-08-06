faadf7edc75918d7102638afd8b1d836705e84d4
Carlos Rubio <crubio@acsendo.com>
2019-08-06 07:19:02 -0500

    Manejando el estado de la peticion con la carga y el e

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react'
 import { useInputValue } from '../../hooks/useInputValue'
-import { Form, Input, Button, Title } from './styles'
+import { Form, Input, Button, Title, Error } from './styles'
 
-export const UserForm = ({ onSubmit, title }) => {
+export const UserForm = ({ error, disabled, onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
@@ -16,12 +16,13 @@ export const UserForm = ({ onSubmit, title }) => {
 
   return (
     <Fragment>
-      <Title>{ title }</Title>
-      <Form onSubmit={handleSubmit}>
-        <Input placeholder='Email' {...email} />
-        <Input placeholder='Password' type='password' {...password} />
-        <Button>{ title }</Button>
+      <Form onSubmit={handleSubmit} disabled={disabled}>
+        <Title>{ title }</Title>
+        <Input placeholder='Email' {...email} disabled={disabled} />
+        <Input placeholder='Password' type='password' {...password} disabled={disabled} />
+        <Button disabled={disabled}>{ title }</Button>
       </Form>
+      {error && <Error> {error} </Error>}
     </Fragment>
   )
 }
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -10,13 +10,15 @@ export const NotRegisteredUser = () => (
         return <Fragment>
           <RegisterMutation>
             {
-              (register) => {
+              (register, { data, loading, error }) => {
                 const onSubmit = ({ email, password }) => {
                   const input = { email, password }
                   const variables = { input }
                   register({ variables }).then(activateAuth)
                 }
-                return <UserForm title='Resgistrarse' onSubmit={onSubmit} />
+
+                const errorMsg = error && 'Hay un error'
+                return <UserForm disabled={loading} error={errorMsg} title='Resgistrarse' onSubmit={onSubmit} />
               }
             }
           </RegisterMutation>
